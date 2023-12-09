71ddc465d1f83811eda5d23f7a77d6f9ad392300
Jakob <jakob.frenzel@hotmail.com>
2023-12-09 15:58:23 +0100

    mods

--- build_exe.bat
+++ build_exe.bat
@@ -4,13 +4,21 @@ setlocal
 REM Set the paths
 set SOURCE_FOLDER=.
 set BUILD_FOLDER=%SOURCE_FOLDER%\build
-set EXE_NAME=strider_ps4-client.exe
+set VERSION_FILE=%SOURCE_FOLDER%\version_info.py
+set EXE_NAME_PREFIX=strider_ps4-client
+set ICON_FILE=%SOURCE_FOLDER%\icon.ico
 
 REM Change to the source folder
 cd %SOURCE_FOLDER%
 
+set CLIENT_VERSION_MAJOR=0
+set CLIENT_VERSION_MINOR=0
+
+REM set EXE_NAME=%EXE_NAME_PREFIX%_%CLIENT_VERSION_MAJOR%-%CLIENT_VERSION_MINOR%
+set EXE_NAME=%EXE_NAME_PREFIX%
+
 REM Run PyInstaller to build the exe inside the build folder
-py -m PyInstaller --onefile --distpath %BUILD_FOLDER% strider_ps4-client.py --icon icon.ico
+py -m PyInstaller --onefile --distpath %BUILD_FOLDER% --name %EXE_NAME% --icon %ICON_FILE% strider_ps4-client.py
 
 REM Check if PyInstaller was successful
 if not exist %BUILD_FOLDER%\%EXE_NAME% (
@@ -19,7 +27,7 @@ if not exist %BUILD_FOLDER%\%EXE_NAME% (
 )
 
 REM Copy the exe back to the source folder
-move /Y %BUILD_FOLDER%\%EXE_NAME% %SOURCE_FOLDER%
+move /Y %BUILD_FOLDER%\%EXE_NAME%.exe %SOURCE_FOLDER%
 
 echo Build successful!
 
